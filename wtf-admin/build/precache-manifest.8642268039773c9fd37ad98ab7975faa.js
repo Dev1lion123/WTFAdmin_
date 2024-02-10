9fd98f40a955286794fc5a35364c12b18abe2434
Dmitriy <93647407+1swaN@users.noreply.github.com>
2024-02-10 20:49:03 +0400

    4.0

---
+++ wtf-admin/build/precache-manifest.8642268039773c9fd37ad98ab7975faa.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e2c7433084f7bce86230cd04bb56cca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f77eed94c8f54c62c5a",
+    "url": "/static/css/main.2c737bc0.chunk.css"
+  },
+  {
+    "revision": "f1e92664cb41252390f1",
+    "url": "/static/js/2.5e73c84f.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/static/js/2.5e73c84f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8dd00c0ccf6aad5056fa",
+    "url": "/static/js/3.9ec8676c.chunk.js"
+  },
+  {
+    "revision": "9f77eed94c8f54c62c5a",
+    "url": "/static/js/main.a6f90792.chunk.js"
+  },
+  {
+    "revision": "5a21f6fe8355d040c1d9",
+    "url": "/static/js/runtime-main.5efe9345.js"
+  },
+  {
+    "revision": "2b2b7da40e9fe37746226683d444215a",
+    "url": "/static/media/getFetch.2b2b7da4.cjs"
+  }
+]);
